91fb3ee78207a1a9ada19b0047cd01941478e67c
sasicodes <me@sasi.codes>
2022-01-26 19:17:21 +0530

    Added test cases

--- test/gog-test.ts
+++ test/gog-test.ts
@@ -3,9 +3,14 @@ import { ethers } from 'hardhat'
 describe('GOG Contract', function () {
   it('should log address of contract', async function () {
     const GOG = await ethers.getContractFactory('GOG')
-    const GOGReceipt = await GOG.deploy()
-    await GOGReceipt.deployed()
+    const contract = await GOG.deploy()
+    await contract.deployed()
 
-    console.log('GOG Contract - ', GOGReceipt.address)
+    console.log('GOG Contract - ', contract.address)
+
+    const mint = await contract.mintItem()
+    console.log('🚀 ~ file: gog-test.ts ~ line 12 ~ mint', mint)
+    const token = await contract.tokenURI(1)
+    console.log('🚀 ~ file: gog-test.ts ~ line 13 ~ token', token)
   })
 })
